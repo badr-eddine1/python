6ee8ed0528ed2df369402cd5d491328acb7e6387
badr eddine <badreddineaitbenijja@gmail.com>
2026-09-03 18:28:45 +0100

    panierexecice

---
+++ panier.py
@@ -0,0 +1,10 @@
+produits = {
+    "ordi" : 3500,
+    "telephone" : 1500,
+    "tablette" : 800,
+    "chargeur" : 200
+}
+print("_____menu_____")
+print ("afficher les produits disponibles")
+print("ajouté un produits au panier ")
+print("supprimer un produits du panier ")
